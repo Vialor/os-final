424322bd4247d6db9b1c6b7d28c8da8429522cae
cbxoi <2938142976@qq.com>
2023-12-03 21:11:17 -0500

    .

--- system_call.c
+++ system_call.c
@@ -7,7 +7,8 @@
 #include "rdwr.h"
 
 const int Rea_Size = 167772160;
-int block_size = 1024, block_count;
+const int MiB = 1<<20;
+int block_size = 1, block_count;
 char File_name[] = "test_file.txt";
 int start_time, finish_time;
 
@@ -16,12 +17,13 @@ int main(){
   start_time = time(NULL);
   block_read(File_name, block_size, Rea_Size/block_size);
   finish_time = time(NULL);
-  printf("First Read Runtime: %d\n", finish_time-start_time);
-  
-  start_time = time(NULL);
-  block_read(File_name, block_size, Rea_Size/block_size);
-  finish_time = time(NULL);
-  printf("Again Read Runtime: %d\n", finish_time-start_time);
 
+  double MiBps = (double)Rea_Size / (double)MiB / (double)(finish_time-start_time);
+  printf("MiB/s when block_size=1 : %.6lf\n", MiBps );
+
+  double Bps = (double)Rea_Size / (double)(finish_time-start_time);
+  print("Number of System Calls in 1 second: %.6lf\n", Bps);
+
+  
   return 0;
 }
