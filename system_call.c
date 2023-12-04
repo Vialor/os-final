50e35e30868975ed220bd394c09d09d1b2a5a104
cbxoi <2938142976@qq.com>
2023-12-03 21:20:50 -0500

    .

--- system_call.c
+++ system_call.c
@@ -6,10 +6,11 @@
 #include <stdlib.h>
 #include "rdwr.h"
 
-const int Rea_Size = 167772160;
+const int Rea_Size = 167772160 >> 3;
 const int MiB = 1<<20;
 int block_size = 1, block_count;
 char File_name[] = "test_file.txt";
+int file_size = 1<<20;
 int start_time, finish_time;
 
 int main(){
@@ -22,8 +23,19 @@ int main(){
   printf("MiB/s when block_size=1 : %.6lf\n", MiBps );
 
   double Bps = (double)Rea_Size / (double)(finish_time-start_time);
-  print("Number of System Calls in 1 second: %.6lf\n", Bps);
+  print("Number of read in 1 second: %.6lf\n", Bps);
 
   
+  //Test of lseek
+  int fd;
+  while((fd = open(File_name, O_RDONLY) ) <1);
+  srand(time(NULL));
+  start_time = time(NULL);
+  for(int i=0; i<Rea_Size; i++)
+    lseek(fd, rand()%file_size, SEEK_SET);
+  finish_time = time(NULL);
+  Bps = (double)Rea_Size / (double)(finish_time-start_time);
+  print("Number of lseek in 1 second: %.6lf\n", Bps);
+  close(fd);
   return 0;
 }
