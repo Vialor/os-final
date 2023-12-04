e569dd4bd62eb01a5e292f98b0e87d4d943b9913
cbxoi <2938142976@qq.com>
2023-12-03 20:59:21 -0500

    .

--- caching.c
+++ caching.c
@@ -9,11 +9,19 @@
 const int Rea_Size = 167772160;
 int block_size = 1024, block_count;
 char File_name[] = "test_file.txt";
+int start_time, finish_time;
 
 int main(){
-  int fd;
-  while((fd = open(File_name, O_RDONLY) ) <3);
 
-  int 
+  start_time = time(NULL);
+  block_read(File_name, block_size, Rea_size/block_size);
+  finish_time = time(NULL);
+  printf("First Read Runtime: %d\n", finish_time-start_time);
+  
+  start_time = time(NULL);
+  block_read(File_name, block_size, Rea_size/block_size);
+  finish_time = time(NULL);
+  printf("First Read Runtime: %d\n", finish_time-start_time);
+  
   return 0;
 }
