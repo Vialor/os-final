4e3d107289a45eb56ecba35560220aa3a1b7c341
Vialor <vial0520@gmail.com>
2023-12-09 02:23:53 +0800

    clean up warning

--- fast.c
+++ fast.c
@@ -12,7 +12,7 @@
 
 const int MiB = 1<<20;
 int block_size = 4096, block_count;
-char File_name[256];
+char file_name[256];
 double start_time, finish_time;
 unsigned int res[num_threads];
 long long filesize = 0;
@@ -23,7 +23,7 @@ double now() {
   return tv.tv_sec + tv.tv_usec / 1000000.0;
 }
 
-unsigned int XOR(unsigned int  *A, int size){
+unsigned int XOR(unsigned int *A, int size){
   unsigned int res = 0;
   for(int i=0; i<size; i++){
     /*
@@ -37,19 +37,19 @@ unsigned int XOR(unsigned int  *A, int size){
   return res;
 }
 
-void * apply_read(void *arg){
-  int num = (int) arg;
-  int fd = open(File_name, O_RDONLY);
+void* apply_read(void *arg){
+  int num = *(int *)arg;
+  int fd = open(file_name, O_RDONLY);
   if(fd<0){
     printf("File Open Failed!\n");
-    return;
+    pthread_exit(NULL);
   }
 
   int size;
   
   if(lseek(fd, num*block_size, SEEK_SET) == -1){
     close(fd);
-    return;
+    pthread_exit(NULL);
   }
   int step = (num_threads-1)*block_size;
 
@@ -59,13 +59,13 @@ void * apply_read(void *arg){
   filesize += size;
   if(size < block_size){
     close(fd);
-    return;
+    pthread_exit(NULL);
   }
 
   while(1){
     if(lseek(fd, step, SEEK_CUR) == -1){
       close(fd);
-      return;
+      pthread_exit(NULL);
     }
     size = read(fd, buf, block_size);
     res[num] ^= XOR((unsigned int *)buf, size/4);
@@ -77,7 +77,7 @@ void * apply_read(void *arg){
     //printf("current filesize: %lld\n", filesize);
     if(size < block_size){
       close(fd);
-      return;
+      pthread_exit(NULL);
     }
   }
 }
@@ -88,7 +88,7 @@ int main(int argc, char *argv[]){
     write(2, err, sizeof(err));
     return 0;
   }
-  strcpy(File_name, argv[1]);
+  strcpy(file_name, argv[1]);
 
   pthread_t *threads;
   threads = (pthread_t *) malloc(sizeof(pthread_t)*num_threads);
@@ -100,8 +100,11 @@ int main(int argc, char *argv[]){
 
   start_time = now();
 
-  for(int i=0; i<num_threads; i++)
-    pthread_create(&threads[i], NULL, apply_read, (void *)i);
+  for(int i=0; i<num_threads; i++) {
+    int *arg = malloc(sizeof(*arg));
+    *arg = i;
+    pthread_create(&threads[i], NULL, apply_read, arg);
+  }
 
   for(int i=0; i<num_threads; i++) {
     pthread_join(threads[i], NULL);
